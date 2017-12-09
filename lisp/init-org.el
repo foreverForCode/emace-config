7a6b140ae6752a2c55e76c34f0cfc84d015a673c
lixiang <freewm@163.com>
2017-12-09 11:57:33 +0800

    add org-agenda

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,9 +1,12 @@
 ;;  π”√org
 (require 'org)
 
+(require 'org-agenda)
+
 (setq org-src-fontify-natively t)
 
 (setq org-agenda-files '("~/org"))
 
 (global-set-key (kbd "C-c a") 'org-agenda)
+
 (provide 'init-org)
