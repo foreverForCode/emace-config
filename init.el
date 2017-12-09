1f9e3be514d73ecee85ff7a036edb0d6c01d5391
lixiang <freewm@163.com>
2017-12-09 11:38:44 +0800

    finish moduel

--- init.el
+++ init.el
@@ -4,46 +4,13 @@
 (require 'package)
 
 (package-initialize)
-
-
-;; 不显示GNU emacs启动界面
-
-(setq inhibit-startup-message t)
-
-;; 关闭光标样式
-
-(setq-default cursor-type 'bar)
-
-
-;; 不显示菜单栏
-
-(menu-bar-mode 0)
-
-;; 不显示工具栏
-
-(tool-bar-mode 0)
-
-;; 不显示滚动条
-
-(scroll-bar-mode 0)
-
-;; 设置tab为4个空格的宽度
-
-(setq default-tab-width 4)
-
-;; 设置字体和字体大小
-
-(set-default-font "Consolas-14")
-
-
-
-
-
 ;;=====================================================
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 ;; package management
 ;;-------------------------------
+
+(require 'init-ui)
 
 (require 'init-packages)
 
