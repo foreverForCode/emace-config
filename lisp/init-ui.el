f66fb67d8a55889caff362785dfeb2fc4c41d455
李想 <lixiang@lixiangdeMacBook-Pro.local>
2018-03-13 19:21:34 +0800

    modify font

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -25,6 +25,15 @@
 
 ;; 设置字体和字体大小
 
+(set-face-attribute
+ 'default nil :font "Monaco 14")
+;; Chinese Font 配制中文字体
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font (frame-parameter nil 'font)
+                    charset
+                    (font-spec :family "Kaiti SC" :size 14)))
+
+
 ;; ---------------------------------------------------------------------------Kaiti SC" to "Microsoft YaHei" or other fonts  
 
 (provide 'init-ui)
